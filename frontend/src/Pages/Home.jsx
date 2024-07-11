c7e79057ecb740d86666a03aa5730a2668bb0ab9
chaetesh <MY_NAME@example.com>
2024-07-11 16:01:01 +0530

    missionupdate

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -4,11 +4,8 @@ import { Carousel } from "../Components/Carousel";
 import { slides } from "../data/carouselData.json";
 import About from "../Components/About";
 import Services from "../Components/Services";
-<<<<<<< HEAD
 import Footer from '../Components/Footer';
-=======
 import Mission from '../Components/Mission';
->>>>>>> d31ae14 (mission&benfits)
 
 const Home = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
@@ -22,12 +19,9 @@ const Home = () => {
       <Carousel data={slides} />
       <About isDarkMode={isDarkMode}/>
       <Services isDarkMode={isDarkMode} />
-<<<<<<< HEAD
       <Footer isDarkMode={isDarkMode}/>
-=======
       <Mission></Mission>
       
->>>>>>> d31ae14 (mission&benfits)
     </div>
   )
 }
